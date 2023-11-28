1b12b99875c7b1ebd80114e45f0230ecc9787efe
prfwork <prfwork@gmail.com>
2023-11-28 15:46:23 -0300

    Update deploy.sh
    modifico el deploy.sh para que copie los jar

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,4 +1,7 @@
-ls
+ca target
+
+s3 sync ./ s3://$S3_ORIGIN_BUCKET --include "*.jar" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
+
 
 # Sync bundles with strong cache
 #aws s3 sync ./common/favicons s3://$S3_ORIGIN_BUCKET/common/favicons --exclude "site.webmanifest" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
